41800418262901a76ad453bec7edea4e7522185c
Matthew A. Smith <matthew@digivation.net>
2017-07-20 23:48:36 -0700

    Fixed issue with spaces in path Added quotes to variables

--- bin/mount-iso.sh
+++ bin/mount-iso.sh
@@ -2,7 +2,7 @@
 MOUNT_BASE=/mnt/iso
 
 # Get disk name
-ISO_NAME=`isoinfo -d -i $1 | awk 'FNR == 3 {print $3}'`
+ISO_NAME=`isoinfo -d -i "$1" | awk 'FNR == 3 {print $3}'`
 echo "DISK: $ISO_NAME"
 echo "FILE: $1"
 
@@ -10,16 +10,16 @@ echo "FILE: $1"
 if `mount | grep -qs "$1"`; then
     MOUNT_PATH=`mount | grep /home/matthew/xpl10_nodvd.iso | awk '{print $3}'`
     echo "$1 is mounted, unmounting..."
-    umount $1
+    umount "$1"
 
     echo "...removing directory $MOUNT_PATH"
-    rmdir $MOUNT_PATH
+    rmdir "$MOUNT_PATH"
 else
     # Get image title (for path)
-    ISO_NAME=`isoinfo -d -i $1 | awk 'FNR == 3 {print $3}'`
+    ISO_NAME=`isoinfo -d -i "$1" | awk 'FNR == 3 {print $3}'`
 
     echo "$1 is not mounted, mounting..."
     echo "...creating directory $MOUNT_BASE/$ISO_NAME"
-    mkdir $MOUNT_BASE/$ISO_NAME
-    mount -o loop $1 $MOUNT_BASE/$ISO_NAME
+    mkdir "$MOUNT_BASE/$ISO_NAME"
+    mount -o loop "$1" "$MOUNT_BASE/$ISO_NAME"
 fi
